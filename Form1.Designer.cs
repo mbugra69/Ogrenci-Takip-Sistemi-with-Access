519f8e90f8def2335442b1a05e79d6792212dce4
mbugra69 <42447501+mbugra69@users.noreply.github.com>
2021-07-26 01:58:18 +0300

    1.0
    guncelleme

---
+++ Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿
+namespace Ogrenci_Takip_Projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.radioBtnErkek = new System.Windows.Forms.RadioButton();
+            this.radioBtnKiz = new System.Windows.Forms.RadioButton();
+            this.cBSinif = new System.Windows.Forms.ComboBox();
+            this.dg = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel3);
+            this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(348, 425);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Öğrenci Bilgileri";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.txtAd, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtSoyad, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtNumara, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cBSinif, 1, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 21);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(330, 308);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyadı:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cinsiyeti:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sınıfı:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Numarası:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtAd.Location = new System.Drawing.Point(118, 19);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(176, 22);
+            this.txtAd.TabIndex = 5;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtSoyad.Location = new System.Drawing.Point(118, 80);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(176, 22);
+            this.txtSoyad.TabIndex = 6;
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtNumara.Location = new System.Drawing.Point(118, 265);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(179, 22);
+            this.txtNumara.TabIndex = 9;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.radioBtnErkek, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.radioBtnKiz, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(118, 125);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(200, 55);
+            this.tableLayoutPanel2.TabIndex = 10;
+            // 
+            // radioBtnErkek
+            // 
+            this.radioBtnErkek.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.radioBtnErkek.AutoSize = true;
+            this.radioBtnErkek.Location = new System.Drawing.Point(3, 17);
+            this.radioBtnErkek.Name = "radioBtnErkek";
+            this.radioBtnErkek.Size = new System.Drawing.Size(65, 21);
+            this.radioBtnErkek.TabIndex = 0;
+            this.radioBtnErkek.TabStop = true;
+            this.radioBtnErkek.Text = "Erkek";
+            this.radioBtnErkek.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnKiz
+            // 
+            this.radioBtnKiz.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.radioBtnKiz.AutoSize = true;
+            this.radioBtnKiz.Location = new System.Drawing.Point(103, 17);
+            this.radioBtnKiz.Name = "radioBtnKiz";
+            this.radioBtnKiz.Size = new System.Drawing.Size(48, 21);
+            this.radioBtnKiz.TabIndex = 1;
+            this.radioBtnKiz.TabStop = true;
+            this.radioBtnKiz.Text = "Kız";
+            this.radioBtnKiz.UseVisualStyleBackColor = true;
+            // 
+            // cBSinif
+            // 
+            this.cBSinif.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.cBSinif.FormattingEnabled = true;
+            this.cBSinif.Items.AddRange(new object[] {
+            "6-A",
+            "6-B",
+            "6-C",
+            "6-D"});
+            this.cBSinif.Location = new System.Drawing.Point(118, 201);
+            this.cBSinif.Name = "cBSinif";
+            this.cBSinif.Size = new System.Drawing.Size(176, 24);
+            this.cBSinif.TabIndex = 11;
+            // 
+            // dg
+            // 
+            this.dg.AllowUserToAddRows = false;
+            this.dg.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Location = new System.Drawing.Point(367, 13);
+            this.dg.Name = "dg";
+            this.dg.ReadOnly = true;
+            this.dg.RowHeadersVisible = false;
+            this.dg.RowHeadersWidth = 51;
+            this.dg.RowTemplate.Height = 24;
+            this.dg.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dg.Size = new System.Drawing.Size(716, 425);
+            this.dg.TabIndex = 1;
+            this.dg.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dg_CellClick);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 4;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.Controls.Add(this.btnEkle, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnGuncelle, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnSil, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnYeni, 3, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(6, 348);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(336, 71);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(3, 3);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 65);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(87, 3);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(78, 65);
+            this.btnGuncelle.TabIndex = 1;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(171, 3);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 65);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.Location = new System.Drawing.Point(255, 3);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(75, 65);
+            this.btnYeni.TabIndex = 3;
+            this.btnYeni.Text = "Yeni";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            this.btnYeni.Click += new System.EventHandler(this.btnYeni_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 558);
+            this.Controls.Add(this.dg);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Öğrenci Takip Sistemi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtNumara;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.RadioButton radioBtnErkek;
+        private System.Windows.Forms.RadioButton radioBtnKiz;
+        private System.Windows.Forms.ComboBox cBSinif;
+        private System.Windows.Forms.DataGridView dg;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnYeni;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace Ogrenci_Takip_Projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection con;
+        OleDbCommand cmd;
+        string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Buğra\source\repos\Ogrenci-Takip-Projesi\Ogrenci-Takip-Projesi\ogrenciler.accdb;";
+        int ogrenciId = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ogrenciLoad();
+        }
+        void ogrenciLoad()
+        {
+
+            try
+            {
+                con = new OleDbConnection(connectionString);
+                cmd = new OleDbCommand("select * from ogrenci", con);
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dg.DataSource = dt;
+                dg.Columns["id"].Visible = false;
+
+            }
+            catch(OleDbException ex) 
+            {
+                throw ex;
+            }
+        }
+        void ekle()
+        {
+            try
+            {
+                con = new OleDbConnection(connectionString);
+                cmd = new OleDbCommand("insert into ogrenci(adi,soyadi,cinsiyeti,sinifi,numarasi) values (@adi,@soyadi,@cinsiyeti,@sinifi,@numarasi)", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@adi", txtAd.Text);
+                cmd.Parameters.AddWithValue("@soyadi", txtSoyad.Text);
+
+                string cinsiyet = "";
+                if(radioBtnErkek.Checked)
+                {
+                    cinsiyet = "Erkek";
+                } else if(radioBtnKiz.Checked)
+                {
+                    cinsiyet = "Kız";
+                }
+                cmd.Parameters.AddWithValue("@cinsiyeti", cinsiyet );
+                cmd.Parameters.AddWithValue("@sinifi", cBSinif.Text);
+                cmd.Parameters.AddWithValue("@numarasi", txtNumara.Text);
+                cmd.ExecuteNonQuery();
+            }
+            catch(OleDbException ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+            ogrenciLoad();
+
+        }
+
+        void guncelle()
+        {
+            try
+            {
+                con = new OleDbConnection(connectionString);
+                cmd = new OleDbCommand("update ogrenci set adi=@adi,soyadi=@soyadi,cinsiyeti=@cinsiyeti,sinifi=@sinifi,numarasi=@numarasi where id=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@adi", txtAd.Text);
+                cmd.Parameters.AddWithValue("@soyadi", txtSoyad.Text);
+
+                string cinsiyet = "";
+                if (radioBtnErkek.Checked)
+                {
+                    cinsiyet = "Erkek";
+                }
+                else if (radioBtnKiz.Checked)
+                {
+                    cinsiyet = "Kız";
+                }
+                cmd.Parameters.AddWithValue("@cinsiyeti", cinsiyet);
+                cmd.Parameters.AddWithValue("@sinifi", cBSinif.Text);
+                cmd.Parameters.AddWithValue("@numarasi", txtNumara.Text);
+                cmd.Parameters.Add("@id", OleDbType.Integer).Value = ogrenciId;
+                cmd.ExecuteNonQuery();
+            }
+            catch (OleDbException ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+            ogrenciLoad();
+
+        }
+
+
+
+        void sil()
+        {
+            try
+            {
+                con = new OleDbConnection(connectionString);
+                cmd = new OleDbCommand("delete from ogrenci  where id=@id", con);
+                con.Open();
+      
+                cmd.Parameters.Add("@id", OleDbType.Integer).Value = ogrenciId;
+                cmd.ExecuteNonQuery();
+            }
+            catch (OleDbException ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+            ogrenciLoad();
+
+        }
+
+
+        void temizle()
+        {
+            foreach (var item in tableLayoutPanel1.Controls)
+            {
+                if(item is TextBox)
+                {
+                    ((TextBox)item).Text = "";
+                }
+                if (item is ComboBox)
+                {
+                    ((ComboBox)item).Text = "";
+                }
+
+            }
+
+            radioBtnErkek.Checked = false;
+            radioBtnKiz.Checked = false;
+         
+        }
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            ekle();
+        }
+
+        private void dg_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+          if(e.RowIndex > -1) // eğer verilerden başka yere tıklarsak sorun çıkmasın diye. veriler 0' dan başlıyor.
+            {
+                ogrenciId = Convert.ToInt32(dg.Rows[e.RowIndex].Cells["id"].Value);
+                try
+                {
+                    con = new OleDbConnection(connectionString);
+                    cmd = new OleDbCommand("select * from ogrenci where id = @id", con);
+                    cmd.Parameters.Add("@id", OleDbType.Integer).Value = ogrenciId;
+
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    radioBtnErkek.Checked = false;
+                    radioBtnKiz.Checked = false;
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        txtAd.Text = row["adi"].ToString();
+                        txtSoyad.Text = row["soyadi"].ToString();
+                        string cinsiyet = row["cinsiyeti"].ToString();
+                        if(cinsiyet == "Erkek")
+                        {
+                            radioBtnErkek.Checked = true;
+                        } 
+                        else if(cinsiyet == "Kız")
+                        {
+                            radioBtnKiz.Checked = true;
+                        }
+                        cBSinif.Text = row["sinifi"].ToString();
+                        txtNumara.Text = row["numarasi"].ToString();
+
+
+                    }
+                }
+                catch(OleDbException ex)
+                {
+                    throw ex;
+                }
+
+
+
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+
+            if (ogrenciId > 0)
+            {
+
+           
+            guncelle();
+            }
+            else
+            {
+                MessageBox.Show("Öğrenci Seçiniz.", "Uyarı!");
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (ogrenciId > 0)
+            {
+                DialogResult dialogResult = MessageBox.Show("Gerçekten bu kaydı silmek istediğinize emin misiniz?", "Uyarı!", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+
+                    sil();
+                }
+                else
+                {
+                    MessageBox.Show("İşlem iptal edildi.", "Uyarı!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Öğrenci Seçiniz.", "Uyarı!");
+            }
+        }
+
+        private void btnYeni_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+    }
+}
